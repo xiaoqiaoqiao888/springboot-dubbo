2cb17c645b1713cc9f66cc4258201b534345743f
xiaoqiaoqiao888 <qiaodongjie666@163.com>
2018-06-04 09:15:15 +0800

    修改

--- springboot-dubbo-consumer/src/main/java/com/camelot/service/ConsumerService.java
+++ springboot-dubbo-consumer/src/main/java/com/camelot/service/ConsumerService.java
@@ -5,9 +5,10 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ConsumerService{
+public class ConsumerService {
     @Reference
     private ProvideService provideService;
+
     public String sayHello(String name) {
         return provideService.sayHello(name);
     }
