4ffe3f5155ccf4e898b49fe693f66295567a16c8
乔栋杰 <qiaodj@asp.citic.com>
2019-04-19 20:01:01 +0800

    修改bug

--- springboot-dubbo-provider/src/main/java/com/rails/service/impl/ProvideServiceImpl.java
+++ springboot-dubbo-provider/src/main/java/com/rails/service/impl/ProvideServiceImpl.java
@@ -3,9 +3,6 @@ package com.rails.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.rails.service.ProvideService;
 
-import org.springframework.stereotype.Component;
-
-@Component
 @Service
 public class ProvideServiceImpl implements ProvideService {
 
